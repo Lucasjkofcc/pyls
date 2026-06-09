b68372e4c4a62d67570479d2e7cc984a71d8547a
Lucasjkofcc <brawllucas15@gmail.com>
2026-06-08 21:36:16 -0300

    directory view added

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
-a simples ls with -a made in python
+a simples ls with -l made in python
 
-this works only for the path that you are if you use python pyls.py somedir/ it will not work 
 
 TODO:
-    add suport to view contents in folders
+    add suport to view .files contents in folderssssssssss
--- pyls.py
+++ pyls.py
@@ -10,3 +10,15 @@ if len(args) < 2:
 elif args[1] == '-a':
     for f in os.listdir("."):
         print(f)
+elif args[1] != '-a':
+    os.chdir(args[1])
+    for file in os.listdir():
+        if file[0] == '.':
+            pass
+        else:
+            print(file)
+elif args[1] != '-a' and args[2] == "-a":
+    os.chdir(args[1])
+    for file in os.listdir():
+        if file in os.listdir():
+            print(file)
